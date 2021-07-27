fd4a3d01837871c9956c00b3f1c5c768858aa5f1
Raniconduh <clagv.randomgames@gmail.com>
2021-07-26 21:10:33 -0500

    Support for new among-sus

--- sus-ui.py
+++ sus-ui.py
@@ -1,12 +1,9 @@
 import curses
 import socket
-from multiprocessing.dummy import Pool
+import json
 
 
-pool = Pool(processes=1)
-inpline = ""
-
-
+# read all data in socket
 def readall(s):
     s.settimeout(0.25)
     buf = ""
@@ -17,18 +14,23 @@ def readall(s):
     except socket.timeout:
         return buf
 
+# read single line in socket
 def readline(s):
-    s.settimeout(None)
+    s.settimeout(0.1)
     buf = ''
-    while c := s.recv(1):
-        if c == b'\n':
-            return buf
-        try: buf += c.decode('utf-8')
-        except UnicodeDecodeError: pass
+    try:
+        while c := s.recv(1):
+            if c == b'\n':
+                return buf
+            try: buf += c.decode('utf-8')
+            except UnicodeDecodeError: pass
+    except socket.timeout:
+        return None
+    screen.erase()
     return buf
 
 
-def get_serv_info(screen):
+def _get_server_info(screen):
     screen.erase()
     screen.refresh()
     curses.echo()
@@ -39,86 +41,188 @@ def get_serv_info(screen):
 
     screen.addstr(3, 1, "Server Port [1234]:")
     port = screen.getstr(4, 1)
-
-    screen.addstr(5, 1, "Username:")
-    uname = screen.getstr(6, 1)
-
+    
     curses.noecho()
     curses.curs_set(0)
 
-    ip = ip.decode('utf-8') if ip else 'localhost'
-    port = int(port.decode('utf-8')) if port else 1234
-    uname = uname
+    try: ip = ip.decode('utf-8') if ip else 'localhost'
+    except UnicodeDecodeError: return False, False
 
-    return ip, port, uname
+    try: port = int(port.decode('utf-8')) if port else 1234
+    except Exception: return False, False
+
+    return ip, port
 
 
-def getchcb(c):
-    global inpline
-    if c: inpline += chr(c)
+# get server host and port
+def get_server_info(screen):
+    host, port = _get_server_info(screen)
+    while not host or not port:
+        screen.erase()
+        screen.addstr(1, 1, 'Invalid host or port entered')
+        screen.refresh()
+        curses.napms(1000)
+        host, port = _get_server_info(screen)
+
+    return host, port
+
+
+def _connect_to_socket(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try: s.connect((host, port))
+    except Exception: return False
+
+    return s
+
+
+# connect to the socket
+def connect_to_socket(screen, host, port):
+    s = _connect_to_socket(host, port)
+    while not s:
+        screen.erase()
+        screen.addstr(1, 1, "Could not connect to socket")
+        screen.refresh()
+        curses.napms(1000)
+        host, port = get_server_info(screen)
+        s = _connect_to_socket(host, port)
+    
+    return s
+
+ 
+def _get_uname(screen):
+    screen.erase()
+    curses.echo()
+    curses.curs_set(1)
+
+    screen.addstr(1, 1, "Enter your username:")
+    uname = screen.getstr(2, 1)
+
+    curses.noecho()
+    curses.curs_set(1)
+
+    if not uname:
+        return False
+    
+    try: uname = uname.decode('utf-8')
+    except Exception: return False
+
+    return uname
+
+
+# get username
+def get_uname(screen):
+    uname = _get_uname(screen)
+    while not uname:
+        screen.erase()
+        screen.addstr(1, 1, "Invalid username")
+        screen.refresh()
+        curses.napms(1000)
+        uname = _get_uname(screen)
+    return uname
 
 
 def main(screen):
-    global pool
-    global inpline
+    host, port = get_server_info(screen)
+    s = connect_to_socket(screen, host, port)
 
-    host, port, uname = get_serv_info(screen)
-    screen.erase()
-    screen.refresh()
+    name = get_uname(screen)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))    
+    welcome = readall(s)
+    welcome = json.loads(welcome)
 
-    s.send(uname + b'\n')
+    nd = {"type":"name", "arguments":{"name": name}}
+    nd = json.dumps(nd)
 
-    row = 1
-    column = 1
+    s.send(nd.encode('utf-8'))
+
+    name_response = readall(s)
+    name_response = json.loads(name_response)
+
+    name_stauses = {
+            0: "Name Too Short",
+            1: "Name Too Long",
+            2: "Invalid Name",
+            3: "Name Taken"
+    }
+
+    # invalid name
+    while name_response["type"] != "greeting":
+        screen.erase()
+        screen.addstr(1, 1, name_stauses[name_response["status"]])
+        screen.refresh()
+        curses.napms(1000)
+        
+        name = get_uname(screen)
+        nd = json.dumps({"type":"name", "arguments":{"name":name}})
+        s.send(nd.encode('utf-8'))
+
+        name_response = json.loads(readall(s))
 
     chat_buf = []
 
-    first_message = readall(s).split('\n')[:-2]
-    for line in first_message:
-        chat_buf.append(line)
-        screen.addstr(row, column, line)
-        row += 1
-    screen.refresh()
+    row = 1
+    col = 1
 
-    admin = False
+    max_height, max_width = screen.getmaxyx()
 
-    if first_message[1][-3:] == ' 0!':
-        admin = True
+    screen.timeout(0)
+    
+    inpline = ""
 
-    if admin:
-        chat_buf.append("You are the admin")
-        screen.addstr(row, column, "You are the admin")
-        row += 1
-
+    # chat
     while True:
-        #screen.erase()
-        for line in readall(s).split('\n'):
-            if line: chat_buf.append(line)
+        screen.erase()
 
+        mov_curs_flag = False
+
+        if line := readline(s):
+            line = json.loads(line)
+            if line["type"] == "chat":
+                sender = line["arguments"]["player"]
+                msg = line["arguments"]["message"]
+                chat_buf.append(f'[{sender}]: {msg}')
+            if line["type"] == "join":
+                player = line["arguments"]["player"]
+                chat_buf.append(f'[{player} joined the lobby]')
+            if line["type"] == "leave":
+                player = line["arguments"]["player"]
+                chat_buf.append(f'[{player} left the lobby]')
+            else:
+                chat_buf.append(f'**Unknown server response** [{line["type"]}]')
+
+        # handle inputs
+        if (c := screen.getch()) != -1:
+            if c == ord('\n'):
+                new_msg = {"type":"message", "arguments":{"message":inpline}}
+                new_msg = json.dumps(new_msg)
+
+                s.send(new_msg.encode('utf-8'))
+
+                chat_buf.append(f'[you]: {inpline}')
+                inpline = ""
+            elif c == ord('\b') or c == 127:
+                inpline = inpline[:-1]
+                mov_curs_flag = True
+            else:
+                inpline += chr(c)
+
+        if len(chat_buf) == max_height - 3:
+            chat_buf = chat_buf[1:]
+
+        # print chat buffer to screen
         for line in range(len(chat_buf)):
-            if chat_buf[line]:
-                screen.addstr(line + 1, column, chat_buf[line])
+            screen.addstr(line + 1, col, chat_buf[line])
 
-        max_height, max_width = screen.getmaxyx()
-
-        pool.apply_async(screen.getch, callback=getchcb)
-        if inpline and inpline[-1] == '\n':
-            chat_buf.append(f'*you*: {inpline}')
-            screen.addstr(row, column, chat_buf[-1])
-            row += 1
-            s.send(bytes(inpline, 'utf-8'))
-            inpline = ""
-            screen.erase()
-        elif inpline and ord(inpline[-1]) == 127:
-            inpline = inpline[:-2]
-
-        screen.addstr(max_height - 2, 1, inpline+ ' ')
+        if mov_curs_flag:
+            screen.addstr(max_height - 2, col, inpline + ' ')
+            y, x = screen.getyx()
+            screen.move(y, x - 1)
+        else:
+            screen.addstr(max_height - 2, col, inpline)
 
         screen.refresh()
-        
+
 
 if __name__ == '__main__':
     screen = curses.initscr()
@@ -128,6 +232,7 @@ if __name__ == '__main__':
     
     screen.clear()
     screen.refresh()
+
     try:
         main(screen)
     except KeyboardInterrupt:
