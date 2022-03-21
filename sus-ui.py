9b5e1aeea0b71dc3ae1bd513e739121324e2690d
rani <clagv.randomgames@gmail.com>
2022-03-20 22:54:14 -0500

    added basic support for new server; overhauled code into main class for Thread()

--- sus-ui.py
+++ sus-ui.py
@@ -3,6 +3,49 @@
 import curses
 import socket
 import json
+from threading import Thread
+
+
+class JType():
+    # server -> client
+    S_INFO    = 0     # server
+    C_INFO    = 1     # client
+    GAME_STATUS = 2
+    ROOM_INFO = 3
+    STATE     = 4     # client state
+    TASKS     = 5     # tasks list
+    # client -> server
+    COMMAND   = 6     # run command
+    NAME      = 7     # set name
+    LOCATION  = 8     # set location
+    # client <-> server
+    CLIENTS   = 9     # request clients
+    CHAT      = 10    # send chat
+    TASK      = 11    # do task
+
+
+class SCode():
+    # generic
+    GEN_OK     = 0    # OK response
+    GEN_INVAL  = 1    # invalid packet
+    GEN_AGAIN  = 2    # packet resent
+    # name
+    NAME_WLEN  = 3    # name bad length
+    # game
+    GAME_NOTIN = 4    # client not playing
+    GAME_WROLE = 5    # wrong role
+    GAME_WLOC  = 6    # wrong location
+    #client
+    CLI_SJOIN   = 0   # server join
+    CLI_SLEAVE  = 1   # server leave
+    CLI_RENTER  = 2   # room enter
+    CLI_RLEAVE  = 3   # room leave
+    CLI_VOTE    = 4   # voted out
+    # game status
+    GSTAT_FULL    = 0 # game full
+    GSTAT_RUNNING = 1 # game started
+    GSTAT_IMPWIN  = 2 # imposter victory
+    GSTAT_CREWWIN = 3 # crew victory
 
 
 sock_closed = False
@@ -15,7 +58,7 @@ def readall(s):
         while c := s.recv(1):
             try: buf += c.decode('utf-8')
             except UnicodeDecodeError: pass
-    except BlockingIOError:
+    except socket.timeout:
         pass
     return buf
 
@@ -29,7 +72,7 @@ def readline(s):
                 return buf
             try: buf += c.decode('utf-8')
             except UnicodeDecodeError: pass
-    except BlockingIOError:
+    except socket.timeout:
         return None
     if not buf:
         sock_closed = True
@@ -128,17 +171,155 @@ def get_uname(screen):
     return uname
 
 
+class BlockHandler():
+    # {id: "name", ...}
+    clients = {}
+    tasks = []
+    doors = []
+    location = ""
+    role = -1
+
+    msg_buf = []
+
+    game_playing = False
+
+    def __init__(self, sock, screen):
+        self.s = sock
+        self.screen = screen
+
+        y, x = self.screen.getmaxyx()
+
+        self.msg_box = self.screen.subwin(y - 2, x - 1, 1, 1)
+        self.inp_box = self.screen.subwin(1, x - 1, y - 1, 1)
+
+        self.msg_box.erase()
+        self.msg_box.refresh()
+
+        self.inp_box.erase()
+        self.inp_box.refresh()
+
+    def sock_getline(self): # read line from socket
+        line = b''
+        while (c := self.s.recv(1)) != b'\n': line += c
+        try: return line.decode('utf-8')
+        except Exception: return None
+
+    def screen_getline(self): # read line of user input
+        line = self.inp_box.getstr(0, 0)
+        self.inp_box.erase()
+        self.inp_box.refresh()
+        try: return line.decode('utf-8')
+        except Exception: return None
+
+    def message(self, s): # print to message buffer
+        self.msg_buf.append(s)
+        y, x = self.msg_box.getmaxyx()
+        if len(self.msg_buf) == y:
+            self.msg_buf = self.msg_buf[1:]
+        self.msg_box.erase()
+        for line in range(len(self.msg_buf)):
+            self.msg_box.addstr(line, 1, self.msg_buf[line])
+        self.msg_box.refresh()
+
+    # quit the game
+    def quit_game(self):
+        pass
+
+    def socket_handler(self):
+        while line := self.sock_getline():
+            line = json.loads(line)
+            # server info - WIP
+            if line["type"] == JType.S_INFO:
+                pass
+            #client info - WIP
+            elif line["type"] == JType.C_INFO:
+                if "name" in line["arguments"]: player = line["arguments"]["name"]
+                else: player = None
+                p_id = line["arguments"]["id"]
+                if line["status"] == SCode.CLI_SJOIN:
+                    self.clients[p_id] = player
+                    self.message(f'[{player} joined the game]')
+                elif line["status"] == SCode.CLI_SLEAVE:
+                    del self.clients[p_id]
+                    self.message(f'[{player} left the game]')
+                elif line["status"] == SCode.CLI_RENTER:
+                    self.message(f'[{self.clients[p_id]} entered the room]')
+                elif line["status"] == SCode.CLI_RLEAVE:
+                    self.message(f'[{self.clients[p_id]} left the room]')
+            # game status - WIP
+            elif line["type"] == JType.GAME_STATUS:
+                pass
+            # room info - WIP
+            elif line["type"] == JType.ROOM_INFO:
+                pass
+            # game state - WIP
+            elif line["type"] == JType.STATE:
+                if "role" in line["arguments"]:
+                    self.role = line["arguments"]["role"]
+                    r = ""
+                    if self.role == 0: r = "a crewmate"
+                    elif self.role == 1: r = "an imposter"
+                    self.message(f'You are {r}')
+            # tasks - WIP
+            elif line["type"] == JType.TASKS:
+                for task in line["arguments"]:
+                    self.tasks.append(task)
+            # command failed
+            elif line["type"] == JType.COMMAND:
+                c = line["arguments"]["name"]
+                self.message(f'** Could not run command ({c}) **')
+            # list clients
+            elif line["type"] == JType.CLIENTS:
+                for client in line["arguments"]:
+                    self.clients[client["id"]] = client["name"]
+            # chat
+            elif line["type"] == JType.CHAT:
+                if "arguments" in line:
+                    name = self.clients[line["arguments"]["id"]]
+                    message = line["arguments"]["content"]
+                    self.message(f'[{name}]; {message}')
+                elif "status" in line and line["status"] != 0:
+                    self.message("** Couldn't send chat **")
+            # complete task - WIP
+            elif line["type"] == JType.TASK:
+                if line["status"] == SCode.GEN_OK:
+                    pass
+                    #tasks.....
+            else: self.message(f'** Unknown server response ({line}) **')
+
+    # TODO: change to single character input editing
+    # instead of using screen_getline
+    def input_handler(self):
+        while True:
+            line = self.screen_getline()
+            lsplit = line.split(' ')
+            # start game
+            if line == "/start":
+                pass
+            # go to room
+            elif lsplit[0] == "/go" and len(lsplit) > 1:
+                pass
+            # do task
+            elif lsplit[0] == "/do" and len(lsplit) > 1:
+                pass
+            # quit game
+            elif line == "/quit":
+                self.s.close()
+                self.quit_game()
+            elif not self.game_playing:
+                new_msg = {"type":JType.CHAT,"arguments":{"content":line}}
+                new_msg = json.dumps(new_msg)
+                self.s.send(new_msg.encode('utf-8'))
+                self.message(f'<you>: {line}')
+
+
 def main(screen):
     host, port = get_server_info(screen)
     s = connect_to_socket(screen, host, port)
-    s.setblocking(False)
 
     name = get_uname(screen)
 
-    welcome = readall(s)
-    welcome = json.loads(welcome)
-
-    nd = {"type":"name", "arguments":{"name": name}}
+    nd = {"type":JType.NAME, "arguments":{"name":name}}
     nd = json.dumps(nd)
 
     s.send(nd.encode('utf-8'))
@@ -148,37 +329,31 @@ def main(screen):
         name_response = readline(s)
     name_response = json.loads(name_response)
 
-    name_stauses = {
-            0: "Success",
-            1: "Name Too Short",
-            2: "Name Too Long",
-            3: "Invalid Name",
-            4: "Name Taken"
-    }
-
     # invalid name
-    while name_response["status"]:
+    while name_response["status"] != SCode.GEN_OK:
+        err = ""
+        if name_response["status"] == SCode.NAME_WLEN:
+            err = "Name too long or too short"
+        else:
+            err = "Bad name input"
         screen.erase()
-        screen.addstr(1, 1, name_stauses[name_response["status"]])
+        screen.addstr(1, 1, err)
         screen.refresh()
         curses.napms(1000)
         
         name = get_uname(screen)
-        nd = json.dumps({"type":"name", "arguments":{"name":name}})
+        nd = json.dumps({"type":JType.NAME, "arguments":{"name":name}})
         s.send(nd.encode('utf-8'))
         
-        name_response = readall(s)
+        name_response = readline(s)
         while not name_response:
-            name_response = readall(s)
+            name_response = readline(s)
         name_response = json.loads(name_response)
 
     chat_buf = []
 
     row = 1
     col = 1
-
-    # set getch() to non-blocking
-    screen.timeout(0)
 
     inpline = ""
     screen.erase()
@@ -192,143 +367,13 @@ def main(screen):
     location = None
     doors = []
     tasks = []
+    clients = {}
 
     global sock_closed
 
-    # chat
-    while True:
-        max_height, max_width = screen.getmaxyx()
-        mov_curs_flag = False
-        clear_flag = False
-
-        if line := readline(s):
-            line = json.loads(line)
-            if line["type"] == "chat":
-                sender = line["arguments"]["player"]
-                msg = line["arguments"]["message"]
-                chat_buf.append(f'[{sender}]: {msg}')
-            elif line["type"] == "player_status":
-                if line["status"] == 0:
-                    player = line["arguments"]["player"]
-                    chat_buf.append(f'[{player} left the lobby]')
-                elif line["status"] == 1:
-                    player = line["arguments"]["player"]
-                    chat_buf.append(f'[{player} joined the lobby]')
-            elif line["type"] == "game_status":
-                if line["status"] == 0:
-                    game_play = True
-                    new_com = {"type":"location"}
-                    s.send(json.dumps(new_com).encode('utf-8'))
-                    new_com = {"type":"tasks"}
-                    s.send(json.dumps(new_com).encode('utf-8'))
-            elif line["type"] == "player_type":
-                player_type = int(line["status"])
-                tmp_type = player_types[player_type]
-                chat_buf.append(f'[You are {"an" if player_type == 1 else "a"} {tmp_type}]')
-            elif line["type"] == "location" and line["status"] == 1:
-                location = line["arguments"]["name"]
-                doors = [door for door in line["arguments"]["doors"]]
-            elif line["type"] == "set_location":
-                pass
-            elif line["type"] == "tasks":
-                tasks = [task for task in line["arguments"]]
-            elif line["type"] == "do_task" and line["status"] == 0:
-                chat_buf.append('**Did task**')
-            elif line["type"] == "set_location" and line["status"] == 0:
-                location = line["arguments"]["name"]
-            else:
-                chat_buf.append(f'**Unknown server response** [{line["type"]}]')
-
-        if sock_closed:
-            return
-
-        # handle inputs
-        if (c := screen.getch()) != -1 and not read_next:
-            if c == ord('\n') and inpline.strip():
-                isplit = inpline.split(' ')
-                if inpline == '/start':
-                    new_com = {"type":"start_game"}
-                    s.send(json.dumps(new_com).encode('utf-8'))
-                elif inpline == '/location':
-                    new_com = {"type":"location"}
-                    s.send(json.dumps(new_com).encode('utf-8'))
-                elif isplit[0] == '/go' and len(isplit) > 1:
-                    new_com = {"type":"set_location","arguments":{"name":' '.join(isplit[1:])}}
-                    s.send(f'{json.dumps(new_com)}\n'.encode('utf-8'))
-
-                    new_com = {"type":"location"}
-                    s.send(json.dumps(new_com).encode('utf-8'))
-                elif inpline == "/tasks":
-                    new_com = {"type": "tasks"}
-                    s.send(json.dumps(new_com).encode('utf-8'))
-                elif isplit[0] == '/do' and len(isplit) > 1:
-                    new_com = {"type":"do_task","arguments":{"name":' '.join(isplit[1:]),"location":location}}
-                    s.send(f'{json.dumps(new_com)}\n'.encode('utf-8'))
-                    
-                    new_com = {"type": "tasks"}
-                    s.send(f'{json.dumps(new_com)}\n'.encode('utf-8'))
-                elif inpline == "/quit":
-                    s.close()
-                    return
-                elif not game_play:
-                    new_msg = {"type":"message","arguments":{"message":inpline}}
-                    new_msg = json.dumps(new_msg)
-                    s.send(new_msg.encode('utf-8'))
-
-                    chat_buf.append(f'<you>: {inpline}')
-
-                inpline = ""
-                clear_flag = True
-            elif c == ord('\n') and not inpline.strip():
-                y, x = screen.getyx()
-                screen.move(y - 1, x)
-            elif c == ord('\b') or c == 127: # DEL
-                inpline = inpline[:-1]
-                mov_curs_flag = True
-            elif c == 27: # ESC
-                read_next = 3
-            else:
-                inpline += chr(c)
-
-        if read_next: read_next -= 1
-
-        if len(chat_buf) == max_height - 4:
-            chat_buf = chat_buf[1:]
-            clear_flag = True
-
-        if clear_flag:
-            screen.erase()
-
-        # print chat buffer to screen
-        for line in range(len(chat_buf)):
-            screen.addstr(line + 3, col, chat_buf[line])
-
-        # print location and doors to top of screen
-        if location and doors:
-            screen.addstr(1, 1, f'Location: {location}  Doors: {" ".join(doors)}')
-        else:
-            screen.addstr(1, 1, f'Lobby')
-        
-        # display tasks
-        if tasks:
-            screen.addstr(1, max_width - 45, "Tasks:")
-            for task in range(len(tasks)):
-                done = '-' if tasks[task]["done"] else ' '
-                taskd = tasks[task]["description"]
-                loc = tasks[task]["location"]
-                screen.addstr(task + 2, max_width - 45, f'[{done}] {taskd} in {loc}')
-        elif game_play:
-            new_com = {"type":"tasks"}
-            s.send(json.dumps(new_com).encode('utf-8'))
-
-        if mov_curs_flag:
-            screen.addstr(max_height - 2, col, inpline + ' ')
-            y, x = screen.getyx()
-            screen.move(y, x - 1)
-        else:
-            screen.addstr(max_height - 2, col, inpline)
-
-        screen.refresh()
+    handlers = BlockHandler(s, screen)
+    Thread(target=handlers.socket_handler).start()
+    Thread(target=handlers.input_handler).start()
 
 
 if __name__ == '__main__':
